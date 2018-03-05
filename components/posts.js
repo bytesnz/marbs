c66f37f51bc433ba62362057f1a898cf4834bbdb
Jack Farley <or4e3dsw@bytes.nz>
2018-03-05 08:21:40 +0000

    Add full details list to Posts component

--- components/posts.js
+++ components/posts.js
@@ -1,16 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
 const react_redux_1 = require("react-redux");
-const PostsComponent = ({ posts, limit, filter, actions }) => {
-    if (posts === null) {
-        actions.posts.fetchPosts();
-        return null;
-    }
-    if (posts.data) {
-        posts = posts.data;
-    }
-    else {
-        return null;
+const react_router_dom_1 = require("react-router-dom");
+const urlJoin = require("join-path");
+const utils_1 = require("../lib/utils");
+const config_1 = require("../app/lib/config");
+const PostsComponent = ({ posts, _posts, limit, filter, actions, full }) => {
+    if (typeof posts === 'undefined') {
+        if (_posts === null) {
+            actions.posts.fetchPosts();
+            return null;
+        }
+        if (typeof _posts.data === 'undefined') {
+            return null;
+        }
+        posts = _posts.data;
     }
     if (filter) {
         posts = filter(posts);
@@ -18,9 +23,20 @@ const PostsComponent = ({ posts, limit, filter, actions }) => {
     if (limit) {
         posts = posts.slice(0, limit);
     }
-    return (React.createElement("ul", { className: "posts" }, posts.map((post) => (React.createElement("li", { key: post.id },
-        React.createElement(react_router_dom_1.Link, { to: urlJoin(config_1.default.baseUri, post.id) }, post.attributes.title))))));
+    if (full) {
+        return (React.createElement("ul", { className: "posts full" }, posts.map((post) => (React.createElement("li", { key: post.id, className: post.attributes.categories ?
+                utils_1.flattenCategories(post.attributes.categories).join(' ') : '' },
+            post.attributes.date ? (React.createElement("time", { dateTime: post.attributes.date }, (new Date(post.attributes.date)).toLocaleDateString())) : null,
+            React.createElement("h1", null,
+                React.createElement(react_router_dom_1.Link, { to: urlJoin(config_1.default.baseUri, post.id) }, post.attributes.title)),
+            post.attributes.tags ? (React.createElement("div", { className: "tags" }, post.attributes.tags.map((tag) => (React.createElement(react_router_dom_1.Link, { key: tag, to: utils_1.tagUrl(tag) }, utils_1.tagLabel(tag)))))) : null,
+            post.attributes.categories ? (React.createElement("div", { className: "categories" }, utils_1.flattenCategories(post.attributes.categories).map((category) => (React.createElement(react_router_dom_1.Link, { key: category, to: utils_1.categoryUrl(category) }, utils_1.categoryLabel(category)))))) : null)))));
+    }
+    else {
+        return (React.createElement("ul", { className: "posts" }, posts.map((post) => (React.createElement("li", { key: post.id },
+            React.createElement(react_router_dom_1.Link, { to: urlJoin(config_1.default.baseUri, post.id) }, post.attributes.title))))));
+    }
 };
 exports.Posts = react_redux_1.connect((state) => ({
-    posts: state.posts
+    _posts: state.posts
 }))(PostsComponent);
--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -2,19 +2,28 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import * as urlJoin from 'join-path';
+import {
+  categoryLabel,
+  categoryUrl,
+  flattenCategories,
+  tagLabel,
+  tagUrl,
+} from '../lib/utils';
 
 import config from '../app/lib/config';
 
-const PostsComponent = ({ posts, limit, filter, actions }) => {
-  if (posts === null) {
-    actions.posts.fetchPosts();
-    return null;
-  }
+const PostsComponent = ({ posts, _posts, limit, filter, actions, full }) => {
+  if (typeof posts === 'undefined') {
+    if (_posts === null) {
+      actions.posts.fetchPosts();
+      return null;
+    }
 
-  if (posts.data) {
-    posts = posts.data;
-  } else {
-    return null;
+    if (typeof _posts.data === 'undefined') {
+      return null;
+    }
+
+    posts = _posts.data;
   }
 
   if (filter) {
@@ -25,19 +34,61 @@ const PostsComponent = ({ posts, limit, filter, actions }) => {
     posts = posts.slice(0, limit);
   }
 
-  return (
-    <ul className="posts">
-      { posts.map((post) => (
-        <li key={post.id}>
-          <Link to={urlJoin(config.baseUri, post.id)}>
-            {post.attributes.title}
-          </Link>
-        </li>
-      )) }
-    </ul>
-  );
+  if (full) {
+    return (
+      <ul className="posts full">
+        { posts.map((post) => (
+          <li key={post.id} className={
+              post.attributes.categories ?
+              flattenCategories(post.attributes.categories).join(' ') : ''
+          }>
+            { post.attributes.date ? (
+              <time dateTime={post.attributes.date}>
+                {(new Date(post.attributes.date)).toLocaleDateString()}
+              </time>
+            ) : null }
+            <h1>
+              <Link to={urlJoin(config.baseUri, post.id)}>
+                {post.attributes.title}
+              </Link>
+            </h1>
+            { post.attributes.tags ? (
+              <div className="tags">
+                {post.attributes.tags.map((tag) => (
+                  <Link key={tag} to={tagUrl(tag)}>
+                    {tagLabel(tag)}
+                  </Link>
+                ))}
+              </div>
+            ) : null }
+            { post.attributes.categories ? (
+              <div className="categories">
+                {flattenCategories(post.attributes.categories).map((category) => (
+                  <Link key={category} to={categoryUrl(category)}>
+                    {categoryLabel(category)}
+                  </Link>
+                ))}
+              </div>
+            ) : null }
+          </li>
+        )) }
+      </ul>
+    );
+  } else {
+    return (
+      <ul className="posts">
+        { posts.map((post) => (
+          <li key={post.id}>
+            <Link to={urlJoin(config.baseUri, post.id)}>
+              {post.attributes.title}
+            </Link>
+          </li>
+        )) }
+      </ul>
+    );
+  }
 };
 
 export const Posts = connect((state) => ({
-  posts: state.posts
+  _posts: state.posts
 }))(PostsComponent);
