c812e70ea08ffe534f4244ccf7b12470b31c13fd
Jack Farley <or4e3dsw@bytes.nz>
2018-06-07 21:44:44 +0100

    Add media handler
    - Scans for images and stores media metadata and galleries in cache
      based on md5 hash of filepath
    - Serves images based on md5 hash of filepath
    - Serves list of images in a given gallery

--- src/lib/handlers/index.ts
+++ src/lib/handlers/index.ts
@@ -1,12 +1,15 @@
 import { contentHandlerCreator as content } from './content';
+import { handlerCreator as media } from './media';
 
 export {
-  content
+  content,
+  media
 };
 
 /**
  * Default content handlers loaded in MARSS
  */
 export default {
-  content
+  content,
+  media
 };
