2c6b8825d8b81539396df1fe8cf3a1600dadacbb
Jack Farley <or4e3dsw@bytes.nz>
2018-07-18 07:43:57 +0100

    Add media handler
    # Comments from tickings:
    # Added stuff to git
    # Working on media handler
    #

---
+++ src/lib/handlers/media.tests.ts
@@ -0,0 +1,211 @@
+import * as Configs from '../../../typings/configs';
+
+import test from 'ava'
+
+require('node-require-alias').setAlias({
+  Config: '../config.global.js'
+});
+
+import { vol } from '../../tests/lib/unionfs';
+
+import { handlerCreator } from './media';
+
+import * as testData from '../../tests/data/source';
+import {
+  createDocument
+} from '../../tests/data/source';
+import { getReturn } from '../../tests/lib/asyncValue';
+
+import * as path from 'path';
+// Import unioned fs
+import * as fs from 'fs';
+
+import * as md5 from 'md5';
+
+import * as sinon from 'sinon';
+
+const testFileHashes = {
+  'image.png': md5('image.png'),
+  'image2.png': md5('image2.png'),
+  'dir/image.png': md5('dir/image.png'),
+  'dir/subdir/image.png': md5('dir/subdir/image.png'),
+  '': md5(''),
+  'dir': md5('dir'),
+  'dir/subdir': md5('dir/subdir')
+};
+
+const mediaCollection = {
+  id: testFileHashes[''],
+  path: '',
+  subGalleries: {
+    [testFileHashes['dir']]: {
+      id: testFileHashes['dir'],
+      path: 'dir',
+      subGalleries: {
+        [testFileHashes['dir/subdir']]: {
+          id: testFileHashes['dir/subdir'],
+          path: 'dir/subdir',
+          subGalleries: {},
+          media: {
+            [testFileHashes['dir/subdir/image.png']]: {
+              width: 9,
+              height: 9,
+              type: 'png',
+              id: testFileHashes['dir/subdir/image.png'],
+              path: 'dir/subdir/image.png',
+              gallery: testFileHashes['dir/subdir']
+            }
+          }
+        }
+      },
+      media: {
+        [testFileHashes['dir/image.png']]: {
+          width: 9,
+          height: 9,
+          type: 'png',
+          id: testFileHashes['dir/image.png'],
+          path: 'dir/image.png',
+          gallery: testFileHashes['dir']
+        }
+      }
+    }
+  },
+  media: {
+    [testFileHashes['image.png']]: {
+      width: 9,
+      height: 9,
+      type: 'png',
+      id: testFileHashes['image.png'],
+      path: 'image.png',
+      gallery: testFileHashes['']
+    },
+    [testFileHashes['image2.png']]: {
+      width: 9,
+      height: 9,
+      type: 'png',
+      id: testFileHashes['image2.png'],
+      path: 'image2.png',
+      gallery: testFileHashes['']
+    }
+  }
+};
+
+const media = {
+  ...mediaCollection.media,
+  ...mediaCollection.subGalleries[testFileHashes['dir']].media,
+  ...mediaCollection.subGalleries[testFileHashes['dir']].subGalleries[testFileHashes['dir/subdir']].media
+};
+
+const testConf: Configs.ServerConfig = {
+  title: 'Test Site',
+  baseUri: '/',
+  address: '127.0.0.1',
+  port: 4321,
+  source: '/source',
+  functionality: {},
+  draftRegex: '\\.draft$',
+  disableFileWatch: true
+};
+
+
+
+let count = 0;
+
+test.beforeEach((t) => {
+  const mockBase = `/${process.pid}-${count++}`;
+  const mockBaseAssets = `${mockBase}/static`
+
+  const testConfig = Object.assign({}, testConf, {
+    staticAssets: mockBaseAssets
+  });
+
+  // Copy test image into test filesystem
+  const image = fs.readFileSync(path.resolve(__dirname, '../../tests/data/image.png'));
+
+  vol.mkdirSync(mockBase);
+  vol.mkdirSync(mockBaseAssets);
+  vol.mkdirSync(path.join(mockBaseAssets, 'dir'));
+  vol.mkdirSync(path.join(mockBaseAssets, 'dir/subdir'));
+  vol.writeFileSync(path.join(mockBaseAssets, 'image.png'), image);
+  vol.writeFileSync(path.join(mockBaseAssets, 'image2.png'), image);
+  vol.writeFileSync(path.join(mockBaseAssets, 'dir/image.png'), image);
+  vol.writeFileSync(path.join(mockBaseAssets, 'dir/subdir/image.png'), image);
+
+  t.context.config = testConfig;
+});
+
+test('that the creator populates the media database', async (t) => {
+  const handler = await handlerCreator(t.context.config);
+
+  t.deepEqual(media, handler.media);
+
+  t.deepEqual(mediaCollection, handler.galleries);
+});
+
+test('that the media event handler returns only media that is in the given directory', async (t) => {
+  const handler = await handlerCreator(t.context.config);
+
+  const id = 1;
+
+  handler.events.media({
+    emit: (type, data) => {
+      t.deepEqual(Object.values(mediaCollection.media).map((media) => ({
+          id: media.id,
+          type: media.type,
+          width: media.width,
+          height: media.height
+        })), data.media);
+      t.deepEqual(id, data.id);
+      t.deepEqual([ mediaCollection.id ], data.ids);
+    }
+  }, {
+    id,
+    ids: mediaCollection.id
+  });
+});
+
+test('media event handler returns media from galleries and sub galleries when subdirectories set', async (t) => {
+  const handler = await handlerCreator(t.context.config);
+
+  const id = 1;
+
+  handler.events.media({
+    emit: (type, data) => {
+      t.deepEqual([
+        ...Object.values(mediaCollection.subGalleries[testFileHashes['dir']].media),
+        ...Object.values(mediaCollection.subGalleries[testFileHashes['dir']].subGalleries[testFileHashes['dir/subdir']].media)
+      ].map((media) => ({
+        id: media.id,
+        type: media.type,
+        width: media.width,
+        height: media.height
+      })), data.media);
+      t.deepEqual(id, data.id);
+      t.deepEqual([ testFileHashes['dir'], testFileHashes['dir/subdir'] ], data.ids);
+    }
+  }, {
+    id,
+    subdirectories: true,
+    ids: testFileHashes['dir']
+  });
+});
+
+test('media path handler returns the requested image', async (t) => {
+  const handler = await handlerCreator(t.context.config);
+
+  const res: any = {};
+
+  res.sendFile = sinon.stub().returns(res);
+  res.status = sinon.stub().returns(res);
+  res.end = sinon.stub().returns(res);
+
+  handler.paths.get['/:id']({
+    params: {
+      id: testFileHashes['image.png']
+    }
+  }, res);
+
+  t.is(1, res.sendFile.callCount, 'sendFile not called');
+  t.log('path was', res.sendFile.getCall(0).args);
+  t.is(path.join(t.context.config.staticAssets, 'image.png'), res.sendFile.getCall(0).args[0], 'sent file not image');
+});
---
+++ src/lib/handlers/media.ts
@@ -0,0 +1,242 @@
+import * as Handlers from '../../../typings/handlers';
+import { ServerConfig } from '../../../typings/configs';
+import * as path from 'path';
+import * as glob from 'glob-promise';
+import { watch } from 'chokidar';
+import * as md5 from 'md5';
+import * as aSizeOf from 'image-size';
+import * as util from 'util';
+import * as urlJoin from 'join-path';
+
+const sizeOf = util.promisify(aSizeOf);
+
+/**
+ * Media (image) file
+ */
+interface Media {
+  id: string,
+  width: number,
+  height: number,
+  path: string,
+  gallery: string
+};
+
+/**
+ * Gallery (folder)
+ */
+interface Gallery {
+  /// MD5 Hash of gallery
+  id: string,
+  /// Path of gallery
+  path: string,
+  /// Ids of galleries underneath this gallery
+  subGalleries: { [hash: string]: Gallery },
+  /// Media contained within this gallery
+  media: { [hash: string]: Media }
+};
+
+/**
+ * Data to retrieve media in a gallery
+ */
+interface MediaQuery {
+  /// ID of query
+  id: string | number,
+  /// Hash of gallery
+  ids: string | Array<string>,
+  /// Whether or not to include media of subdirectories of the galleries
+  subdirectories?: boolean
+};
+
+export const handlerCreator: Handlers.HandlerCreator =
+    async (config: ServerConfig) => {
+  // Media database
+  const media: { [hash: string]: Media } = {};
+  const galleries: Gallery = {
+    id: md5(''),
+    path: '',
+    subGalleries: {},
+    media: {}
+  };
+  const galleriesMap: { [hash: string ]: Gallery } = {
+    [md5('')]: galleries
+  };
+
+  const addGallery = async (folderpath) => {
+    const pathParts = folderpath.split('/');
+
+    let gallery = galleries.subGalleries;
+    let i = 1;
+
+    while (i <= pathParts.length) {
+      const galleryPath = pathParts.slice(0, i).join('/');
+      const galleryHash = md5(galleryPath);
+      if (gallery[galleryHash]) {
+        gallery = gallery[galleryHash].subGalleries;
+      } else {
+        galleriesMap[galleryHash] = {
+          id: galleryHash,
+          path: galleryPath,
+          subGalleries: {},
+          media: {}
+        };
+        gallery[galleryHash] = galleriesMap[galleryHash];
+        gallery = galleriesMap[galleryHash].subGalleries;
+      }
+      i++;
+    }
+  };
+
+  const handleMedia = async (filepath) => {
+    const hash = md5(filepath);
+
+    const pathParts = path.parse(filepath);
+
+    const galleryHash = md5(pathParts.dir);
+
+    // Get the image dimensions
+    const dimensions = await sizeOf(path.join(config.staticAssets, filepath));
+
+    media[hash] = {
+      ...dimensions,
+      id: hash,
+      path: filepath,
+      gallery: galleryHash
+    };
+
+    // Create the image gallery
+    if (pathParts.dir) {
+      addGallery(pathParts.dir);
+    }
+
+    galleriesMap[galleryHash].media[hash] = media[hash];
+  };
+
+  const removeMedia = (filepath) => {
+    const hash = md5(filepath);
+
+    if (typeof media[hash] !== 'undefined') {
+      delete media[hash];
+    }
+  };
+
+  const imageFilesGlob = '**/*.@(jpg|jpeg|gif|png|svg)';
+
+  // Load metadata about media
+  if (!config.disableFileWatch) {
+    console.log('watching for changes in media');
+    watch(imageFilesGlob, {
+      cwd: config.staticAssets
+    }).on('add', (filepath) => {
+      console.log('Adding media', filepath);
+      handleMedia(filepath);
+    }).on('change', (filepath) => {
+      console.log('Updating media', filepath);
+      handleMedia(filepath);
+    }).on('unlink', (filepath) => {
+      console.log('Removing media', filepath);
+      removeMedia(filepath);
+    });
+  } else {
+    console.log('searching for media in', config.staticAssets);
+    await glob(imageFilesGlob, {
+      cwd: config.staticAssets
+    }).then((files) => {
+      return Promise.all(files.map((file) => handleMedia(file)));
+    });
+  }
+
+  return {
+    get media () {
+      return JSON.parse(JSON.stringify(media));
+    },
+    get galleries () {
+      return JSON.parse(JSON.stringify(galleries));
+    },
+    events: {
+      media: (socket, data: MediaQuery) => {
+        if (!data || !data.ids) {
+          socket.emit('media', {
+            error: 'Require a gallery ids'
+          });
+          return;
+        }
+
+        if (!Array.isArray(data.ids)) {
+          data.ids = [ data.ids ];
+        }
+
+        // Check for invalid errors
+        const badGalleries = [];
+
+        data.ids.forEach((id) => {
+          if (!galleriesMap[id]) {
+            badGalleries.push(id);
+          }
+        });
+
+        if (badGalleries.length) {
+          let message;
+
+          if (badGalleries.length === 1) {
+            message = `Gallery ${badGalleries[0]} does not exist`;
+          } else {
+            message = badGalleries.push();
+            message = `Galleries ${badGalleries.join(', ')} and ${message} do no exist`;
+          }
+
+          socket.emit('media', {
+            error: message
+          });
+          return;
+        }
+
+        // Add subdirectories to array
+        if (data.subdirectories) {
+          let allIds = [ ...data.ids ];
+          const galleriesLength = allIds.length;
+
+          const addSubGalleries = (id) => {
+            Object.keys(galleriesMap[id].subGalleries).forEach((subId) => {
+              if (data.ids.indexOf(subId) === -1) {
+                allIds.push(subId);
+              }
+            });
+          };
+
+          allIds.forEach(addSubGalleries);
+
+          data.ids = allIds;
+        }
+
+        let returnedMedia = [];
+
+        // Make array of media
+        data.ids.forEach((id) => {
+          returnedMedia = returnedMedia.concat(Object.values(galleriesMap[id].media));
+        });
+
+        socket.emit('media', {
+          ...data,
+          media: returnedMedia.map((media) => ({
+            id: media.id,
+            type: media.type,
+            width: media.width,
+            height: media.height
+          }))
+        });
+      }
+    },
+    paths: {
+      get: {
+        [urlJoin(config.baseUri, config.staticUri, ':id')]: (req, res, next) => {
+          if (!req.params.id || !media[req.params.id]) {
+            res.status(404).end();
+            return;
+          }
+
+          res.sendFile(path.resolve(path.join(config.staticAssets, media[req.params.id].path)));
+        }
+      }
+    }
+  };
+};
